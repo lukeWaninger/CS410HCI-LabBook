49d2a8e47ae91b45bffd2715148371558bfb4046
luke <luke@SURFACE>
2016-06-24 11:41:29 -0700

    site start

---
+++ CS410HCI-LabBook/CS410HCI-LabBook/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CS410HCI_LabBook.MvcApplication" Language="C#" %>
---
+++ CS410HCI-LabBook/CS410HCI-LabBook/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CS410HCI_LabBook.Startup))]
+namespace CS410HCI_LabBook
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
