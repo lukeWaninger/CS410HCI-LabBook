983f5d5afbeda8f8aa770544fde49b97aca00d30
Luke Waninger <luke.waninger@gmail.com>
2016-07-12 17:28:24 -0700

    fake data for prototype purpose

---
+++ CS410HCI-LabBook/CS410HCI-LabBook/HelperObjects/Tools.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS410HCI_LabBook.HelperObjects {
+    public class DataContent {
+        public List<Section> Sections { get; set; }
+
+        public DataContent() {
+            SetupData();
+        }
+
+
+
+        private void SetupData() {
+            this.Sections = new List<Section>() {
+                new Section {
+                    Id =  1,
+                    Name = "Rakes/Weed Whackers",
+                    ToolList = new List<Tool>() {
+                        new Tool { Id = 1, Name = "A rake" },
+                        new Tool { Id = 2, Name = "A weed whacker" },
+                    }
+                },
+                new Section {
+                    Id = 2,
+                    Name = "Shovels",
+                    ToolList = new List<Tool>() {
+                        new Tool { Id = 3, Name = "A shovel" },
+                        new Tool { Id = 4, Name = "Another shovel" },
+                    },
+                },
+                new Section {
+                    Id = 3,
+                    Name = "Pry Bars",
+                    ToolList = new List<Tool> {
+                        new Tool { Id = 5, Name = "A pry bar" }
+                    }
+                }
+           };
+        }
+    }
+
+    public class Section {
+        public int Id { get; set; }
+        public string Name { get; set; }        
+        public List<Tool> ToolList { get; set; }
+    }
+
+    public class Tool {
+        public int Id { get; set; }
+        public string Name { get; set; }        
+    }
+}
