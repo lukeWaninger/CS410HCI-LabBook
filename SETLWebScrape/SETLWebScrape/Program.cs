30fe0682949a0f9f9a3f6138727f3015f1f2b99e
Luke <Luke@DESKTOP-RRMN87Q>
2016-07-26 11:33:36 -0700

    this is crap and breakable

--- SETLWebScrape/SETLWebScrape/Program.cs
+++ SETLWebScrape/SETLWebScrape/Program.cs
@@ -7,6 +7,7 @@ using System.Windows.Forms;
 using System.Net;
 using Newtonsoft.Json;
 using System.IO;
+using HtmlAgilityPack;
 
 namespace SETLWebScrape {
     class Program {
@@ -29,108 +30,90 @@ namespace SETLWebScrape {
         }
 
         public static void WebBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e) {
+            string localpath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Downloads\\";
+
             WebBrowser browser = sender as WebBrowser;
             browser.DocumentCompleted -= WebBrowser_DocumentCompleted;
 
-            HtmlElementCollection categories = browser.Document.GetElementsByTagName("div");
+                HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+                doc.Load(localpath + "SEPTL.html");
+
+            HtmlNodeCollection categories = doc.DocumentNode.SelectNodes("//*[contains(@class, 'aCategory')]");
             List<Category> categoriesParsed = new List<Category>();
 
             // adding each section
-            foreach (HtmlElement category in categories) {
-                if (category.GetAttribute("classname").ToString() == "aCategory") {
+            List<string> existingTools = new List<string>();
+
+            if (categories != null) {
+                foreach (HtmlNode category in categories) {
                     Category newCat = new Category(category.Id);
 
-                    HtmlElementCollection possibleTools = category.GetElementsByTagName("span");
+                    HtmlNodeCollection possibleTools = category.ChildNodes;
                     List<Tool> toolsParsed = new List<Tool>();
 
                     // adding each tool in the section                  
-                    foreach (HtmlElement item in possibleTools) {
-                        if (item.GetAttribute("classname").ToString() == "tName") {
-                            string id = item.GetAttribute("data-id").ToString();
+                    foreach (HtmlNode item in possibleTools) {
+                        try {
+                            string id = item.ChildNodes[0].GetAttributeValue("data-id", "");
 
-                            string name = item.GetAttribute("innerhtml").ToString();
-                            name = name.Substring(0, name.IndexOf('<')).Replace(@"\", "");
+                            string name = item.ChildNodes[0].InnerHtml;
+                            if (name.Contains("<")) {
+                                name = name.Substring(0, name.IndexOf('<')).Replace(@"\", "");
+                            }
+                            if (name[name.Length - 1] == '(') {
+                                name = name.Substring(0, name.LastIndexOf("(")).Trim();
+                            }
 
-                            #region
-                            //// build post request
-                            //string url = browser.Url.ToString();
-                            //Encoding encoding = Encoding.UTF8;
-                            //byte[] postData = encoding.GetBytes("zid=" + id.ToString());
 
+                            int available = 0;
+                            if (item.ChildNodes[0].InnerText.Contains("Available")) {
+                                available = 1;
+                            }
 
-                            //CookieContainer container = new CookieContainer();
-                            //foreach (string cookie in browser.Document.Cookie.Split(';')) {
-                            //    string nameA = cookie.Split('=')[0];
-                            //    string value = cookie.Substring(name.Length + 1);
-                            //    string path = "/";
-                            //    string domain = "setl.org";
-                            //    container.Add(new Cookie(nameA.Trim(), value.Trim(), path, domain));
+                            //string description, brand, model, lateFine;
+                            //try {
+                            //    description = item.ChildNodes[1].FirstChild.FirstChild.InnerText;
+                            //    description = description.Substring(description.IndexOf(":") + 1);
+
+                            //    brand = item.ChildNodes[1].FirstChild.ChildNodes[2].InnerText;
+                            //    brand = brand.Substring(brand.IndexOf(":") + 1);
+
+                            //    model = item.ChildNodes[1].FirstChild.ChildNodes[3].InnerText;
+                            //    model = model.Substring(model.IndexOf(":") + 1);
+
+                            //    lateFine = item.ChildNodes[1].FirstChild.ChildNodes[4].InnerText;
+                            //    lateFine = lateFine.Substring(lateFine.IndexOf(":") + 1);
                             //}
-
-                            //WebRequest request = WebRequest.Create(url);
-
-                            //HttpWebRequest httpRequest = request as HttpWebRequest;
-                            ////httpRequest.Date = DateTime.Now;
-                            //httpRequest.Accept = "*/*";
-                            //httpRequest.Referer = "http://www.septl.org/TL/ourtools.php";
-                            //System.Net.ServicePointManager.Expect100Continue = false;
-                            //httpRequest.Expect = "200 OK";
-                            //httpRequest.SendChunked = true;
-                            //httpRequest.TransferEncoding = "UTF-8";
-                            //if (httpRequest.CookieContainer == null) {
-                            //    httpRequest.CookieContainer = container;
+                            //catch (Exception ex) {
+                            //    description = "";
+                            //    brand = "";
+                            //    model = "";
+                            //    lateFine = "";
                             //}
-
-                            //request.Method = "POST";
-                            //request.Headers.Add("DNT", "1");
-                            //request.Headers.Add("Accept-Encoding", "gzip, deflate");
-                            //request.Headers.Add("Accept-Language", "en-US,en;q=0.8");
-                            //request.Headers.Add("X-Requested-With", "XMLHttpRequest");
-                            //request.Headers.Add("Origin", "http://www.septl.org");
-                            //request.ContentType = "application/x-www-form-urlencoded; charset=UTF-8";
-                            //request.ContentLength = postData.Length;
-
-
-                            //Stream dataStream = request.GetRequestStream();
-                            //dataStream.Write(postData, 0, postData.Length);
-                            //dataStream.Close();
-                            //WebResponse response = request.GetResponse();
-                            //// Display the status.
-                            //Console.WriteLine(((HttpWebResponse)response).StatusDescription);
-                            //// Get the stream containing content returned by the server.
-                            //dataStream = response.GetResponseStream();
-                            //StreamReader reader = new StreamReader(dataStream);
-                            //string responseFromServer = reader.ReadToEnd();
-                            //Console.WriteLine(responseFromServer);
-                            //reader.Close();
-                            //dataStream.Close();
-                            //response.Close();
-
-                            //browser.Navigate(
-                            //      url,
-                            //      returnData,
-                            //      postData,
-                            //      additionalHeaders
-                            //    );
-                            #endregion
-
-                            newCat.AddTool(new Tool {
-                                id = id,
-                                name = name,
-                            });
-
-                            //// adding tool fields
-                            //HtmlElementCollection possibleFields = item.Parent.GetElementsByTagName("li");
-                            //foreach (HtmlElement field in possibleFields) {
-
-                            //}
+                            if (!existingTools.Contains(name.ToUpper().Trim())) {
+                                newCat.AddTool(new Tool {
+                                    id = id,
+                                    name = name,
+                                    available = available,
+                                    //description = description,
+                                    //brand = brand,
+                                    //lateFine = lateFine,
+                                    //model = model,
+                                });
+                                existingTools.Add(name.ToUpper().Trim());
+                            }
+                            
                         }
+                        catch (Exception ex) { }
                     }
                     categoriesParsed.Add(newCat);
                 }
-            }
+                existingTools.Sort();
+                existingTools.ForEach(x => Console.WriteLine(x));
+                Console.ReadKey();
 
-            System.IO.File.WriteAllText(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Downloads\\SETLTools.txt", JsonConvert.SerializeObject(categoriesParsed));
+                System.IO.File.WriteAllText(localpath + "SETLTools.txt", JsonConvert.SerializeObject(categoriesParsed));
+            }
         }
     }
     public class Category {
@@ -150,5 +133,10 @@ namespace SETLWebScrape {
     public class Tool {
         public string name { get; set; }
         public string id { get; set; }
-    }
+        public int available { get; set; }
+        //public string description { get; set; }
+        //public string brand { get; set; }
+        //public string model { get; set; }
+        //public string lateFine { get; set; }
+    }    
 }
