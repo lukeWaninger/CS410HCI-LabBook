8fc616366cf2b35225769b9ee30643fe1e1e66db
Luke <Luke@DESKTOP-RRMN87Q>
2016-07-26 15:33:23 -0700

    added logging of iframe load times

--- CS410HCI-LabBook/CS410HCI-LabBook/Controllers/CourseProjectController.cs
+++ CS410HCI-LabBook/CS410HCI-LabBook/Controllers/CourseProjectController.cs
@@ -4,38 +4,48 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using CS410HCI_LabBook.ViewModels;
+using System.IO;
 
 namespace CS410HCI_LabBook.Controllers
 {
-    public class CourseProjectController : Controller
-    {
+    public class CourseProjectController : Controller {
         // GET: CourseProject/Ethnography
-        public ActionResult Ethnography()
-        {
+        public ActionResult Ethnography() {
             return View();
         }
 
         // GET: CourseProject/Scenarios
-        public ActionResult Scenarios()
-        {
+        public ActionResult Scenarios() {
             return View();
         }
 
         // GET: CourseProject/DraftLayout
-        public ActionResult DraftLayout()
-        {
+        public ActionResult DraftLayout() {
             return View();
         }
 
         // GET: CourseProject/FocusGroup
-        public ActionResult FocusGroup()
-        {
+        public ActionResult FocusGroup() {
             return View();
         }
 
         // GET: Course Project/UI
-        public ActionResult UI()
-        {
+        public ActionResult UI() {
+            // get average ui interface load time
+            string path = AppDomain.CurrentDomain.BaseDirectory + "Content\\Recourse Documents\\loadTimes.txt";
+            List<double> times = new List<double>();
+            using (StreamReader reader = new StreamReader(path)) {
+                List<string> s = reader.ReadToEnd().Split(';').ToList();
+                
+                foreach (string e in s) {
+                    double time = 0;
+                    if (double.TryParse(e, out time)) {
+                        times.Add(time);
+                    }
+                }
+            }
+
+            ViewBag.AverageLoadTime =  string.Format("{0:0.00} seconds", times.Average());
             return View();
         }
 
@@ -57,8 +67,27 @@ namespace CS410HCI_LabBook.Controllers
             }
             allTools = allTools.Distinct().ToList();
             allTools.OrderBy(t => t);
-            
+
             return Json(allTools, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult LogLoadTime(string duration) {
+            if (string.IsNullOrEmpty(duration)) return Json("failing", JsonRequestBehavior.AllowGet);
+
+            string path = AppDomain.CurrentDomain.BaseDirectory + "Content\\Recourse Documents\\loadTimes.txt";
+            try {
+                using (StreamWriter writer = new StreamWriter(path, true)) {
+                    double seconds = 0;
+                    if (double.TryParse(duration, out seconds)) {
+                        writer.Write(seconds / 1000 + ";");
+                    }
+                }
+            }
+            catch (Exception ex) {
+                return Json("failing", JsonRequestBehavior.AllowGet);
+            }
+            return Json("success", JsonRequestBehavior.AllowGet);
         }
     }
 }
